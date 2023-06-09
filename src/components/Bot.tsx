7018003bbfdb25717ee4373140f832fe6794bed3
Tony <greybluesea@gmail.com>
2023-06-09 18:01:34 +1200

    GPT Bot added

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -5,15 +5,18 @@ type Props = {};
 
 function Bot({}: Props) {
   return (
-    <div>
-      <Image
-        src="/openai-2.svg"
-        /*  src="https://links.papareact.com/c2cdd5" */
-        alt="Trello logo"
-        width={120}
-        height={40}
-        className="w-10 h-10 text-blue inline "
-      />
+    <div className="flex items-center justify-center py-3">
+      <p className="text-sm font-light shadow-md px-5 py-2 md:py-3 rounded-md bg-gray-100 max-w-3xl">
+        <Image
+          src="/openai-2.svg"
+          /*  src="https://links.papareact.com/c2cdd5" */
+          alt="Trello logo"
+          width={120}
+          height={40}
+          className="w-10 h-10 text-blue inline mx-2 "
+        />
+        GPT is summarizing your tasks for the day...
+      </p>
     </div>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,7 +9,7 @@ type Props = {};
 
 function Nav() {
   return (
-    <header className="flex flex-col md:flex-row items-center p-5 bg-gray-300/40 md:justify-between ">
+    <header className="flex flex-col md:flex-row items-center p-5 bg-gray-300/60 md:justify-between ">
       <Image
         src="/trello-logo-worldvectorlogo.svg"
         /*  src="https://links.papareact.com/c2cdd5" */
@@ -19,12 +19,12 @@ function Nav() {
         className="w-34 md:w-44 pb-3 md:pb-0 object-contain "
       />
       <div className="flex items-center space-x-3">
-        <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md ">
+        <form className="flex items-center space-x-5 bg-gray-100 rounded-md p-2 shadow-md ">
           <MagnifyingGlassIcon className="w-6 h-6 text-gray-400 " />
           <input
             type="text"
             placeholder="Search"
-            className="outline-none flex-1 py-1"
+            className="outline-none flex-1 py-1  bg-gray-100"
           />
         </form>
         <UserCircleIcon className="w-8 h-8 text-gray-400 " />
