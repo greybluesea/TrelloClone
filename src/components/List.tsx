5c7741e83e89b3f9af51854f149c2c4fa5a34b08
Tony <greybluesea@gmail.com>
2023-06-12 22:07:47 +1200

    <PlusCircleIcon /> added

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 
@@ -20,44 +21,52 @@ const List = ({ list, index }: Props) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
-          className=" bg-gray-200/90 rounded-lg p-3"
         >
-          <h2 className="font-bold text-lg text-gray-800 ">
-            {statusToTitleObject[list.status]}
-            <span className="text-gray-500 bg-teal-200/80 rounded-full ml-2 px-2 text-sm ">
-              {list.tasks.length}
-            </span>
-          </h2>
-          <Droppable droppableId={list.status} direction="vertical">
-            {(provided, snapshot) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                className={
-                  "pb-2 rounded-lg shadow-sm space-y-2 bg-gray-100/90 " +
-                  (snapshot.isDraggingOver && "bg-green-100/90")
-                }
-              >
-                {list.tasks.map((task, index) => (
-                  <Draggable
-                    key={task.title}
-                    index={index}
-                    draggableId={task.$id}
-                  >
-                    {(provided) => (
-                      <div
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                      >
-                        {task.title}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-              </div>
-            )}
-          </Droppable>
+          <div className=" bg-gray-200/90 rounded-lg p-4 ">
+            <h2 className="font-bold text-lg text-gray-800 pt-1 ">
+              {statusToTitleObject[list.status]}
+              <span className="text-gray-500 bg-teal-200/80 rounded-full ml-2 px-2 text-sm ">
+                {list.tasks.length}
+              </span>
+            </h2>
+            <Droppable droppableId={list.status} direction="vertical">
+              {(provided, snapshot) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className="pt-3"
+                >
+                  {list.tasks.map((task, index) => (
+                    <Draggable
+                      key={task.title}
+                      index={index}
+                      draggableId={task.$id}
+                    >
+                      {(provided) => (
+                        <div
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                          className={
+                            " bg-gray-100/90 rounded-lg shadow-sm mb-2 py-1 " +
+                            (snapshot.isDraggingOver && "bg-green-100/90")
+                          }
+                        >
+                          {task.title}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  <div>
+                    <button>
+                      <PlusCircleIcon className="w-8 h-8 text-gray-700 inline" />
+                    </button>
+                  </div>
+                </div>
+              )}
+            </Droppable>
+          </div>
         </div>
       )}
     </Draggable>
