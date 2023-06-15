725cc18529ea8ec5784c6329b2ab862a96664009
Tony <greybluesea@gmail.com>
2023-06-15 14:20:17 +1200

    try again

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1,13 +1,36 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import useBoardStore from "@/store/boardStore";
 import fetchSummary from "@/lib/fetchSummary";
 
-type Props = {};
+function Bot() {
+  return (
+    <div className="flex items-center justify-center py-3">
+      <div className="shadow-md px-5 py-2 md:py-3 rounded-md bg-gray-200/90 max-w-3xl">
+        <Image
+          src="/openai-2.svg"
+          /*  src="https://links.papareact.com/c2cdd5" */
+          alt="openAI logo"
+          width={120}
+          height={40}
+          className={
+            "w-10 h-10 text-blue inline mx-2 float-left "
+            //  + (isLoading && "animate-spin")
+          }
+        />
+        <p className="text-sm font-light min-w-min  ">
+          GPT is currently not in use, because it's costly.
+        </p>
+      </div>
+    </div>
+  );
+}
 
-function Bot({}: Props) {
-  /*  const board = useBoardStore((state) => state.board);
+export default Bot;
+
+/*  const board = useBoardStore((state) => state.board);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [summary, setSummary] = useState<string>("");
 
@@ -24,29 +47,6 @@ function Bot({}: Props) {
     BotFetchingSummary();
   }, [board]); */
 
-  return (
-    <div className="flex items-center justify-center py-3">
-      <div className="shadow-md px-5 py-2 md:py-3 rounded-md bg-gray-200/90 max-w-3xl">
-        <Image
-          src="/openai-2.svg"
-          /*  src="https://links.papareact.com/c2cdd5" */
-          alt="openAI logo"
-          width={120}
-          height={40}
-          className={
-            "w-10 h-10 text-blue inline mx-2 float-left "
-            /*  + (isLoading && "animate-spin") */
-          }
-        />
-        <p className="text-sm font-light min-w-min  ">
-          {/*  {summary && !isLoading
-            ? summary
-            : "GPT is summarizing your tasks for the day..."} */}
-          GPT is currently not in use, because it's costly.
-        </p>
-      </div>
-    </div>
-  );
-}
-
-export default Bot;
+/*   {summary && !isLoading
+    ? summary
+    : "GPT is summarizing your tasks for the day..."}  */
